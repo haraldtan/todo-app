5d51c813185c9e48afe8c51e59ff92edaa9cea2d
haraldtan <56616203+haraldtan@users.noreply.github.com>
2019-12-27 10:58:25 +0800

    added register page

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -19,7 +19,7 @@ from todo.views import todo, history, contributions, addTodo, deleteTodo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('todo/', todo),
     path('todo/', include('todo.urls')),
 ]
