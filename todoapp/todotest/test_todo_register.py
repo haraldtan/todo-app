f327f3dff9f85aa891857277d4ce623e4ed64ecd
Eshwar72 <44629953+Eshwar72@users.noreply.github.com>
2019-12-27 16:03:11 +0800

    Partial Automated Testing for Register Page

---
+++ todoapp/todotest/test_todo_register.py
@@ -0,0 +1,55 @@
+import pytest
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import select as Select
+
+def test_ExistingUser():
+    driver = webdriver.Firefox()
+    driver.get('http://127.0.0.1:8000/accounts/register/')
+    driver.find_element_by_name("username").send_keys("team5")
+    driver.find_element_by_name("password1").send_keys("Password123")
+    driver.find_element_by_name("password2").send_keys("Password123")
+    submit = driver.find_element_by_name("submit")
+    submit.click()
+    time.sleep(0.5)
+    assert driver.find_element_by_xpath("//*[contains(text(), 'A user with that username already exists.')]")
+
+def test_Valid_150_Charcaters():
+    driver = webdriver.Firefox()
+    driver.get('http://127.0.0.1:8000/accounts/register/')
+    driver.find_element_by_name("username").send_keys("Team5isatngeeannpolytechnic123456789008uyhtgfdcxvbnmefdsrht56rt24ghrwgh8wgh5thj5t8j589hathpe57hwor9aoghbvr89wthfeviFn4w085735trg4hqgnRWUgfgf7efehf8ehf")
+    driver.find_element_by_name("password1").send_keys("Pass123word")
+    driver.find_element_by_name("password2").send_keys("Pass123word")
+    submit = driver.find_element_by_name("submit")
+    submit.click()
+    time.sleep(0.5)
+    assert driver.current_url == 'http://127.0.0.1:8000/accounts/login/'
+
+def test_password_lessthan8Char():
+    driver = webdriver.Firefox()
+    driver.get('http://127.0.0.1:8000/accounts/register/')
+    driver.find_element_by_name("username").send_keys("team52")
+    driver.find_element_by_name("password1").send_keys("Passwor")
+    driver.find_element_by_name("password2").send_keys("Passwor")
+    submit = driver.find_element_by_name("submit")
+    submit.click()
+    time.sleep(0.5)
+    assert driver.find_element_by_xpath("//*[contains(text(), 'This password is too short. It must contain at least 8 characters.')]")
+
+def test_password_allnumeric():
+    driver = webdriver.Firefox()
+    driver.get('http://127.0.0.1:8000/accounts/register/')
+    driver.find_element_by_name("username").send_keys("team52")
+    driver.find_element_by_name("password1").send_keys("12345678")
+    driver.find_element_by_name("password2").send_keys("12345678")
+    submit = driver.find_element_by_name("submit")
+    submit.click()
+    time.sleep(0.5)
+    assert driver.find_element_by_xpath("//*[contains(text(), 'This password is entirely numeric.')]")
+
+
+    
