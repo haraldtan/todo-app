054e88f716c1127775f2e1670266211ff913080f
Isaac <s10177952@connect.np.edu.sg>
2019-12-28 16:02:44 +0800

    Finish 3b,3c,4a

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class TodoItem(models.Model):
     content = models.TextField()
+    time = models.DateTimeField(auto_now=True)
+    info = models.CharField(max_length=50, default='')
+   
 
 class ArchivedItem(models.Model):
     content = models.TextField()
+    time = models.DateTimeField(auto_now=True)
+    info = models.CharField(max_length=50, default='' )
+   
--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import TodoItem, ArchivedItem
 
+
 # Create your views here.
 def todo(request):
     all_todo_items = TodoItem.objects.all()
@@ -18,20 +19,27 @@ def contributions(request):
 
 def addTodo(request):
     new_item = TodoItem(content = request.POST['content'])
-    if new_item != "":
-        new_item.save()
-        return HttpResponseRedirect('/todo/')
-    else:
-        return "This field should not be blank"
+    new_archive_item = ArchivedItem(content = new_item.content , time = new_item.time, info = "Added on") 
+    new_item.save()
+    new_archive_item.save()
+    return HttpResponseRedirect('/todo/')
+   
 
 def deleteTodo(request, todo_id):
     item_to_delete = TodoItem.objects.get(id=todo_id)
     item_to_delete.delete()
+    new_archive_item = ArchivedItem(content = new_item.content , time = new_item.time) 
+    new_item.save()
     return HttpResponseRedirect('/todo/')
 
 def archiveTodo(request, todo_id):
     item_to_archive = TodoItem.objects.get(id=todo_id)
-    new_archive_item = ArchivedItem(content = item_to_archive.content)
+    new_archive_item = ArchivedItem(content = item_to_archive.content , time = item_to_archive.time, info = "Deleted on")
     new_archive_item.save()
     item_to_archive.delete()
     return HttpResponseRedirect('/todo/')
+  
+	
+
+
+	
